42547b2009a1966270d780018200d0ddd76a560d
Marcho <41415464+marchocode@users.noreply.github.com>
2022-12-27 17:29:55 +0800

    Update update-rules-dat.sh

--- update-rules-dat.sh
+++ update-rules-dat.sh
@@ -5,8 +5,8 @@
 
 echo "update start..."
 
-wget -qO geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+wget -qO /usr/share/xray/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
 
-wget -qO geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+wget -qO /usr/share/xray/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
 
 echo "update end..."
